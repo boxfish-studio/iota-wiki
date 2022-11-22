af9068d5fe36cd118c61437100c03163e24c8cf0
Marc Espín <mespinsanz@gmail.com>
2022-11-22 10:39:07 +0100

    Improved the code a bit

--- api/index.js
+++ api/index.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const { renderFile } = require('ejs');
 
 const PORT = process.env.PORT ?? 3000;
+const DOCS_BUILD = __dirname + './../build';
+
 const app = express();
 
-app.engine('html', require('ejs').renderFile);
+app.engine('html', renderFile);
 app.use(express.static('build', { extensions: ['html'] }));
 
 app.use(function (req, res) {
-  res.status(400);
   const isShimmer = req.url.startsWith('/shimmer');
   const isNext = req.url.startsWith('/next');
+
   if (isShimmer) {
-    res.render(__dirname + './../build/shimmer/404.html');
+    res.render(DOCS_BUILD + '/shimmer/404.html');
   } else if (isNext) {
-    res.render(__dirname + './../build/next/404.html');
+    res.render(DOCS_BUILD + '/next/404.html');
   } else {
-    res.render(__dirname + './../build/404.html');
+    res.render(DOCS_BUILD + '/404.html');
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Index server listening on ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
 
 module.exports = app;
