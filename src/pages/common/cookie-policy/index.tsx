424b0471cfcdaac05d0366a533f0d813bb4a96d7
marc2332 <mespinsanz@gmail.com>
2022-11-25 12:17:14 +0100

    feat: Cookie Policy page

---
+++ src/pages/common/cookie-policy/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
+
+export default function CookiePolicy() {
+  return (
+    <>
+      <Head>
+        <script
+          id='CookieDeclaration'
+          src='https://consent.cookiebot.com/8f051d60-4ecb-41a0-abb9-4874fd999e4f/cd.js'
+          type='text/javascript'
+          async
+        ></script>
+      </Head>
+      <Layout>
+        <div className='hero'>
+          <div className='container'>
+            <div className='text--center margin-bottom--lg'>
+              <h1 className='hero__title margin--none'>Cookie Policy</h1>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
