2e0b08024794bc547867b6811c0eadb42805e2f4
Begoña Alvarez <balvarez@boxfish.studio>
2022-11-30 19:48:59 +0100

    enhancement: improve cookie policy page style

--- src/common/pages/cookie-policy/index.js
+++ src/common/pages/cookie-policy/index.js
@@ -4,13 +4,21 @@ import Layout from '@theme/Layout';
 export default function CookiePolicy() {
   return (
     <Layout title='Cookie Policy' description='Cookie Policy'>
-      <h1>Cookie Policy</h1>
-      <script
-        id='CookieDeclaration'
-        src='https://consent.cookiebot.com/8f051d60-4ecb-41a0-abb9-4874fd999e4f/cd.js'
-        type='text/javascript'
-        async
-      ></script>
+      <div className='hero'>
+        <div className='container'>
+          <div className='text--center margin-bottom--lg'>
+            <h1 className='hero__title margin--none'>Cookie Policy</h1>
+          </div>
+          <div className='row'>
+            <script
+              id='CookieDeclaration'
+              src='https://consent.cookiebot.com/8f051d60-4ecb-41a0-abb9-4874fd999e4f/cd.js'
+              type='text/javascript'
+              async
+            ></script>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
